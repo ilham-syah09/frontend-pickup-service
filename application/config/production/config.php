f85b184954668c865bfb7e1f44c88c4c7e0e62e8
bastiannazaromi <bastian.nazaromi@gmail.com>
2023-06-10 11:13:18 +0700

    edit base url

--- application/config/production/config.php
+++ application/config/production/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://frontend.sewadomain.my.id/';
+$config['base_url'] = 'https://frontend-pickupservice.phbcomputerc.my.id';
 
 /*
 |--------------------------------------------------------------------------
